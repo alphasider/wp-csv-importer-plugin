30f0ee236dd1efe9a1a05169cfae1781ca451613
Rustam <rustamergashev.sp@gmail.com>
2020-10-03 05:43:11 +0500

    Fix: create DB on plugin activate

--- csv-importer.php
+++ csv-importer.php
@@ -7,6 +7,24 @@
    * Description: Adds a custom admin pages with sample styles and scripts.
    * Version: 2.0.
    */
+  register_activation_hook(__FILE__, 'create_import_log_table');
+  function create_import_log_table() {
+    global $wpdb;
+    $charset_collate = $wpdb->get_charset_collate();
+    $table_name = $wpdb->prefix . 'ci_import_logs';
+
+    $sql = "CREATE TABLE $table_name (
+        id smallint (5) NOT NULL AUTO_INCREMENT,
+        file_name varchar (50) NOT NULL,
+        import_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        last_modified datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        imported bit (1) DEFAULT NULL,
+        UNIQUE KEY id (id)
+	  ) $charset_collate;";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+  }
 
   function csv_importer_init() {
     require plugin_dir_path(__FILE__) . 'class/AdminPage.php';
